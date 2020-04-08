4c9e1e7a79856d436523227ee206fdf1394d5f61
Nauful <nauful>
2020-04-08 09:39:51 -0500

    Fixed message type for ERR.

--- NET Core/LibUA/Types.cs
+++ NET Core/LibUA/Types.cs
@@ -36,7 +36,7 @@ namespace LibUA
 		{
 			Hello = 0x4C4548,
 			Acknowledge = 0x4B4341,
-			Error = 0x515145,
+			Error = 0x525245,
 			Open = 0x4E504F,
 			Message = 0x47534D,
 			Close = 0x4F4C43,
