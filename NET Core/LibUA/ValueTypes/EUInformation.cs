f6821f0c8a8efc123a211910b6552f9168f6813e
Bjørn F. Esaiassen <bfe@icsys.no>
2024-03-02 20:13:00 +0100

    Added EUInformation value type

---
+++ NET Core/LibUA/ValueTypes/EUInformation.cs
@@ -0,0 +1,64 @@
+﻿using LibUA.Core;
+using System;
+
+namespace LibUA.ValueTypes;
+
+public class EUInformation
+{
+    public string NameSpaceUri { get; set; } = "http://www.opcfoundation.org/UA/units/un/cefact";
+    public int UnitId { get; set; } = -1;
+    public LocalizedText DisplayName { get; set; } = new("");
+    public LocalizedText Description { get; set; } = new("");
+}
+
+public static class EUInformationExtensions
+{
+    public static int CodingSize(this MemoryBuffer mem, EUInformation dv)
+    {
+        int sum = 0;
+
+        sum += mem.CodingSizeUAString(dv.NameSpaceUri);
+        sum += mem.CodingSize(dv.UnitId);
+        sum += mem.CodingSize(dv.DisplayName);
+        sum += mem.CodingSize(dv.Description);
+
+        return sum;
+    }
+
+    public static bool Encode(this MemoryBuffer mem, EUInformation item)
+    {
+        if (!mem.EncodeUAString(item.NameSpaceUri)) { return false; }
+        if (!mem.Encode(item.UnitId)) { return false; }
+        if (!mem.Encode(item.DisplayName)) { return false; }
+        if (!mem.Encode(item.Description)) { return false; }
+
+        return true;
+    }
+
+    public static bool Decode(this MemoryBuffer mem, out EUInformation wv)
+    {
+        wv = null;
+
+        if (!mem.DecodeUAString(out string namespaceUri)) { return false; }
+        if (!mem.Decode(out int unitId)) { return false; }
+        if (!mem.Decode(out LocalizedText displayName)) { return false; }
+        if (!mem.Decode(out LocalizedText description)) { return false; }
+
+        try
+        {
+            wv = new EUInformation()
+            {
+                NameSpaceUri = namespaceUri,
+                UnitId = unitId,
+                DisplayName = displayName,
+                Description = description
+            };
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
