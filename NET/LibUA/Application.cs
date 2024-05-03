d09cd30f1ad8c7d9929d8c764363cbb15c039153
nauful <nauful>
2024-05-03 17:27:16 -0500

    Clean up monitor notification filtering.

--- NET/LibUA/Application.cs
+++ NET/LibUA/Application.cs
@@ -164,20 +164,25 @@ namespace LibUA
                     monitorMapRW.EnterReadLock();
                     if (monitorMap.TryGetValue(key, out List<MonitoredItem> mis))
                     {
-                        for (int i = 0; i < mis.Count; i++)
+                        foreach (MonitoredItem mi in mis)
                         {
-                            if (mis[i].QueueData.Count >= mis[i].QueueSize)
+                            if (!(filterHandler?.Invoke(mi.Parameters?.Filter) ?? true))
                             {
-                                mis[i].QueueOverflowed = true;
-                            }
-                            else if (filterHandler?.Invoke(mis[i].Parameters?.Filter) ?? true)
-                            {
-                                mis[i].QueueData.Enqueue(dv);
+                                continue;
                             }
 
-                            if (mis[i].ParentSubscription.ChangeNotification == Subscription.ChangeNotificationType.None)
+                            if (mi.QueueData.Count >= mi.QueueSize)
                             {
-                                mis[i].ParentSubscription.ChangeNotification = Subscription.ChangeNotificationType.AtPublish;
+                                mi.QueueOverflowed = true;
+                            }
+                            else
+                            {
+                                mi.QueueData.Enqueue(dv);
+                            }
+
+                            if (mi.ParentSubscription.ChangeNotification == Subscription.ChangeNotificationType.None)
+                            {
+                                mi.ParentSubscription.ChangeNotification = Subscription.ChangeNotificationType.AtPublish;
                             }
                         }
                     }
@@ -198,20 +203,25 @@ namespace LibUA
                     monitorMapRW.EnterReadLock();
                     if (monitorMap.TryGetValue(key, out List<MonitoredItem> mis))
                     {
-                        for (int i = 0; i < mis.Count; i++)
+                        foreach (MonitoredItem mi in mis)
                         {
-                            if (mis[i].QueueEvent.Count >= mis[i].QueueSize)
+                            if (!(filterHandler?.Invoke(mi.Parameters?.Filter) ?? true))
                             {
-                                mis[i].QueueOverflowed = true;
-                            }
-                            else if (filterHandler?.Invoke(mis[i].Parameters?.Filter) ?? true)
-                            {
-                                mis[i].QueueEvent.Enqueue(ev);
+                                continue;
                             }
 
-                            if (mis[i].ParentSubscription.ChangeNotification == Subscription.ChangeNotificationType.None)
+                            if (mi.QueueEvent.Count >= mi.QueueSize)
                             {
-                                mis[i].ParentSubscription.ChangeNotification = Subscription.ChangeNotificationType.AtPublish;
+                                mi.QueueOverflowed = true;
+                            }
+                            else
+                            {
+                                mi.QueueEvent.Enqueue(ev);
+                            }
+
+                            if (mi.ParentSubscription.ChangeNotification == Subscription.ChangeNotificationType.None)
+                            {
+                                mi.ParentSubscription.ChangeNotification = Subscription.ChangeNotificationType.AtPublish;
                             }
                         }
                     }
